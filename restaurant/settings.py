d29da69cd5d735a62fc5c8bb0fcaa3e1f43e74fd
Marcos-VNC <marcosvn.cardoso@outlook.com>
2021-10-29 08:09:34 -0300

    make it better

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-19g-nbh+=9u&=ri(tg-kq(n^g6yf)b94dxtbv(8^-xm$)_fcx&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
